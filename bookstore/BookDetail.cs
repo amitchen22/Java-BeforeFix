cdf10c1e48dcb7db476bc16925a34d399047f06b
amitchen22 <amit.chen@checkmarx.com>
2015-08-06 15:51:30 +0300

    Update BookDetail.cs

--- bookstore/BookDetail.cs
+++ bookstore/BookDetail.cs
@@ -3,7 +3,7 @@ namespace Book_Store
 	
 //AMIT4
 //    Filenameyyy: BookDetail.cs
-//    Generated witfdh CodeCharge 2.0.5
+//    Generated witfdh CofgjfjdeCharge 2.0.5
 //    ASP.NET C#.ccp build 03/07/2002
 //hgfjgfjfgjuuufh
 //-------------------------------
